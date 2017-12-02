089ceccdfc05cda169802b089b5a8a445a376e13
Minna <minna.maaria.laitinen@gmail.com>
2017-12-02 16:20:00 +0200

    create-react-native-app

--- index.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native';
-
-import Reportforms from "./src/components/Navigation/AppNavigation";
-
-export default Reportforms;
-
-AppRegistry.registerComponent('testMobApp', () => Reportforms);
-
