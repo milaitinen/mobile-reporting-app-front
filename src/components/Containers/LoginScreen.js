5ad298dfa5bf3cfdb072324c10fb63e478f8e6cb
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2017-12-05 16:13:55 +0200

    Implemented login screen in master

--- src/components/Containers/LoginScreen.js
+++ src/components/Containers/LoginScreen.js
@@ -1,183 +1,210 @@
-import React from 'react';
-import { TouchableOpacity, StyleSheet, Text, TextInput, ImageBackground, } from 'react-native';
+import React from 'react'
+import { TouchableOpacity, StyleSheet, Text, View, Button, TextInput, ScrollView, ImageBackground, Image } from 'react-native'
 
 export default class LoginScreen extends React.Component {
 
-  constructor(props)
-  {
-    super(props);
-    this.state = {
-      isLoading: true,
-      TextInputUser: '',
-      TextInputPassword: '',
-      TextInputServer: ''
-    };
-  }
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            isLoading: true,
+            TextInputUser: '',
+            TextInputPassword: '',
+            TextInputServer: ''
+        };
+    }
 
-  render() {
-    return (
-      <ImageBackground
-        source={require('./img/background_5.png')}
-        style={styles.contentContainer}>
 
-        <Text
-          style={styles.title}>
+    render() {
+        return (
+            <ImageBackground
+                source={require('./img/background_5.png')}
+                style={styles.contentContainer}>
+
+                <Text
+                    style={styles.title}>
                     MR-Application
-        </Text>
+                </Text>
 
-        <Text
-          style={styles.slogan}>
+                <Text
+                    style={styles.slogan}>
                     Keep calm and keep reporting
-        </Text>
+                </Text>
+                <View style={styles.SectionStyle}>
+                    <Image source={require('./img/person.png')} style={styles.ImageStyle}/>
+                    <TextInput
+                        placeholder='Email'
+                        placeholderTextColor='white'
+                        onChangeText={TextInputUser => this.setState({TextInputUser})}
+                        underlineColorAndroid='transparent'
+                        style={styles.TextInputStyleClass}
+                    />
+                </View>
 
-        <TextInput
-          placeholder='Email'
-          placeholderTextColor='white'
-          onChangeText={TextInputUser => this.setState({ TextInputUser })}
-          underlineColorAndroid='transparent'
-          style={styles.TextInputStyleClass}
-        />
+                <View style={styles.SectionStyle}>
+                    <Image source={require('./img/lock.png')} style={styles.ImageStyle}/>
+                    <TextInput
+                        secureTextEntry={true}
+                        placeholder='Password'
+                        placeholderTextColor='white'
+                        onChangeText={TextInputPassword => this.setState({TextInputPassword})}
+                        underlineColorAndroid='transparent'
+                        style={styles.TextInputStyleClass}
+                    />
+                </View>
+                <View style={styles.SectionStyle}>
+                    <Image source={require('./img/language.png')} style={styles.ImageStyle}/>
+                    <TextInput
+                        placeholder='Server url'
+                        placeholderTextColor='white'
+                        onChangeText={TextInputServer => this.setState({TextInputServer})}
+                        underlineColorAndroid='transparent'
+                        style={styles.TextInputStyleClass}
+                    />
+                </View>
 
-        <TextInput
-
-          secureTextEntry={true}
-          placeholder='Password'
-          placeholderTextColor='white'
-          onChangeText={TextInputPassword => this.setState({ TextInputPassword })}
-          underlineColorAndroid='transparent'
-          style={styles.TextInputStyleClass}
-        />
-
-        <TextInput
-          placeholder='Server url'
-          placeholderTextColor='white'
-          onChangeText={TextInputServer => this.setState({ TextInputServer })}
-          underlineColorAndroid='transparent'
-          style={styles.TextInputStyleClass}
-        />
-
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('drawerStack')}
-          style={styles.button}>
-          <Text style={styles.signin}>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate('drawerStack')}
+                    style={styles.button}>
+                    <Text style={styles.signin}>
                         Sign In
-          </Text>
-        </TouchableOpacity>
+                    </Text>
+                </TouchableOpacity>
 
-        <Text
-          style={styles.textLink}
-          onPress={() => this.props.navigation.navigate('forgottenPasswordScreen')} >
+                <Text
+                    style={styles.textLink}
+                    onPress={() => this.props.navigation.navigate('forgottenPasswordScreen')} >
                     Forgot password?
-        </Text>
+                </Text>
 
-        <Text
-          style={styles.signup}
-          onPress={() => this.props.navigation.navigate('signupScreen')} >
+                <Text
+                    style={styles.signup}
+                    onPress={() => this.props.navigation.navigate('signupScreen')} >
                     Sign up
-        </Text>
+                </Text>
 
-        <Text
-          style={styles.copyright}>
+                <Text
+                    style={styles.copyright}>
                     Copyright © Arter Oy 2017
-        </Text>
-      </ImageBackground>
-    );
-  }
+                </Text>
+            </ImageBackground>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
+    ImageStyle: {
+        paddingTop: 10,
+        paddingBottom: 15,
+        paddingRight: 10,
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode : 'stretch',
+        alignItems: 'center'
+    },
 
-  TextInputStyleClass: {
+    SectionStyle: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+        borderBottomWidth: 1,
+        borderBottomColor: 'white',
+        alignSelf: 'center',
+        height: 40,
+        width: 250,
+        margin: 10
+    },
 
-    // textAlign: 'center',
-    width: 250,
-    marginBottom: 15,
-    height: 40,
-    borderBottomWidth: 1,
-    borderBottomColor: 'white',
-    // fontFamily: 'Roboto-Light',
-    fontSize: 16,
-    color: 'white',
-    alignSelf: 'center',
-    // borderWidth: 1,
-    // borderColor: '#FF5722',
+    TextInputStyleClass: {
 
-    // Set border Radius.
-    // borderRadius: 10 ,
-  },
+        //textAlign: 'center',
+        width: 215,
+        height: 40,
+        fontFamily: 'Roboto-Light',
+        fontSize: 16,
+        color: 'white',
+        alignSelf: 'center',
+        //borderWidth: 1,
+        //borderColor: '#FF5722',
 
-  button: {
-    backgroundColor: '#9dcbe5',
-    marginTop:30,
-    paddingTop:10,
-    paddingBottom:10,
-    marginLeft:115,
-    marginRight:115,
-    marginBottom: 25,
-    borderRadius:10,
-    borderWidth: 1,
-    borderColor: '#9dcbe5',
+// Set border Radius.
+        //borderRadius: 10 ,
+    },
 
-  },
+    button: {
+        backgroundColor: '#9dcbe5',
+        marginTop:30,
+        paddingTop:10,
+        paddingBottom:10,
+        marginLeft:115,
+        marginRight:115,
+        marginBottom: 25,
+        borderRadius:10,
+        borderWidth: 1,
+        borderColor: '#9dcbe5',
 
-  signin: {
-    color: '#274752',
-    // fontFamily: 'Roboto-Light',
-    fontSize: 15,
-    textAlign: 'center',
-  },
+    },
 
-  contentContainer: {
-    padding: 20,
-    flex: 1,
-    width: null,
-    height: null,
-    backgroundColor: 'transparent',
-    justifyContent: 'center',
-  },
+    signin: {
+        color: '#274752',
+        fontFamily: 'Roboto-Light',
+        fontSize: 15,
+        textAlign: 'center',
+    },
 
-  textLink: {
-    color: 'white',
-    paddingTop: 20,
-    // fontFamily: 'Roboto-Light',
-    fontSize: 14,
-    textAlign: 'center'
-  },
+    contentContainer: {
+        padding: 20,
+        flex: 1,
+        width: null,
+        height: null,
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+    },
 
-  signup: {
-    color: '#78d3f2',
-    paddingTop: 25,
-    // fontFamily: 'Roboto-Light',
-    fontSize: 15,
-    textAlign: 'center'
-  },
+    textLink: {
+        color: 'white',
+        paddingTop: 20,
+        fontFamily: 'Roboto-Light',
+        fontSize: 14,
+        textAlign: 'center'
+    },
 
-  title: {
-    // fontFamily: 'Roboto-Light',
-    fontSize: 30,
-    textAlign: 'center',
-    marginTop: 10,
-    paddingBottom: 30,
-    color: 'white',
-    // fontWeight: 'bold'
-  },
+    signup: {
+        color: '#78d3f2',
+        paddingTop: 25,
+        fontFamily: 'Roboto-Light',
+        fontSize: 15,
+        textAlign: 'center'
+    },
 
-  slogan: {
-    fontSize: 25,
-    width: 200,
-    textAlign: 'center',
-    paddingBottom: 30,
-    color: 'white',
-    // fontFamily: 'Allura-Regular',
-    alignSelf: 'center',
-    // fontWeight: 'bold'
-  },
+    title: {
+        fontFamily: 'Roboto-Light',
+        fontSize: 30,
+        textAlign: 'center',
+        marginTop: 10,
+        paddingBottom: 30,
+        color: 'white',
+        //fontWeight: 'bold'
+    },
 
-  copyright: {
-    // fontFamily: 'Roboto-Light',
-    color: 'white',
-    textAlign: 'center',
+    slogan: {
+        fontSize: 25,
+        width: 200,
+        textAlign: 'center',
+        paddingBottom: 30,
+        color: 'white',
+        fontFamily: 'Allura-Regular',
+        alignSelf: 'center',
+        //fontWeight: 'bold'
+    },
 
-    marginTop: 50,
-  }
-});
+    copyright: {
+        fontFamily: 'Roboto-Light',
+        color: 'white',
+        textAlign: 'center',
+
+        marginTop: 50,
+    }
+})
