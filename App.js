5f42232cc6b62377b24ee8a6878e4a620c2330ba
Minna <minna.maaria.laitinen@gmail.com>
2018-01-02 22:17:03 +0200

    small styling fixes

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Navigator from './src/navigator/AppNavigation';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Navigator/>
-    );
-  }
+    render() {
+        return (
+            <Navigator/>
+        );
+    }
 }
