28200dff9c25478510eb9a6bbc039e223ecd5af3
Minna <minna.maaria.laitinen@gmail.com>
2018-01-03 14:51:20 +0200

    icons added to TextInput component

--- src/components/TextInput/Input.js
+++ src/components/TextInput/Input.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { View, TextInput, Image } from 'react-native';
+import { View, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import styles from './styles';
 
 const Input = (props) => {
     return (
         <View style={styles.SectionStyle}>
-            <Image
-                source={props.source}
-                style={styles.ImageStyle}
-            />
+            <Icon name={props.name} color={'white'} size={16} />
             <TextInput
                 onChangeText={props.onChangeText}
                 underlineColorAndroid='transparent'
