c33c33a2e3c0285d4f3ed754660b2b290ed816f3
Elisa <elisa.naskali@gmail.com>
2018-01-04 12:52:19 +0200

    Deleted unused code

--- src/screens/TemplateScreen.js
+++ src/screens/TemplateScreen.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import {
-    StyleSheet,
     View,
     FlatList,
-    Platform,
     ActivityIndicator,
     ScrollView,
-    Text
 } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import templateScreenStyles from './style/templateScreenStyles';
