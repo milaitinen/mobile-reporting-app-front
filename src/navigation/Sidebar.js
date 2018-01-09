7bb4b5fcc4605ebeb3912f07e338695eb9fd5b7d
Alvar Wihuri <alvar.wihuri@gmail.com>
2018-01-09 19:18:03 +0200

    Implemented placeholder button for signing out

--- src/navigation/Sidebar.js
+++ src/navigation/Sidebar.js
@@ -1,6 +1,6 @@
 import { NavigationActions } from 'react-navigation';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Button } from 'react-native';
 
 export default class Sidebar extends React.Component {
 
@@ -22,13 +22,8 @@ export default class Sidebar extends React.Component {
     //TODO: update the look
     render () {
         return (
-            <View>
-                <Text onPress={ () => console.log('painoit ekaa nappia')}>
-                    ensimmäinen juttu
-                </Text>
-                <Text onPress={ () => this.logout('loginStack') }>
-                    toka juttu
-                </Text>
+            <View style={ { padding: 20 } }>
+                <Button onPress={() => this.logout('loginStack')} title={'Sign out'}/>
             </View>
         );
     }
