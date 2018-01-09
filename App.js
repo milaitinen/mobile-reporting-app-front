ebd2876389d1ac992a03d16c23701ddd6c660006
Minna <minna.maaria.laitinen@gmail.com>
2018-01-09 22:48:12 +0200

    estylesheet global variable description

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigator from './src/navigation/AppNavigation';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
+// global variables declared here - they can be used anywhere inside src directory.
 EStyleSheet.build({
     // lighter blue: '#455fa1', '#364a7d', '#2e3f6b'
     $darkBlue: '#3d4f7c',
