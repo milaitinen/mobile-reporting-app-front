b27eb44744f9224197bf8e919f44251b26db9038
Minna <minna.maaria.laitinen@gmail.com>
2018-01-19 17:44:09 +0200

    tests added to Layout.js

--- __tests__/NewReportScreen.js
+++ __tests__/NewReportScreen.js
@@ -1,11 +1,11 @@
 import 'react-native';
 import React from 'react';
-import NewReportScreen from '../src/screens/NewReportScreen';
-import { url } from '../src/screens/urlsetting';
-
 import renderer from 'react-test-renderer';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
+import NewReportScreen from '../src/screens/NewReportScreen';
+import { url } from '../src/screens/urlsetting';
 
 configure({ adapter: new Adapter() });
 
@@ -15,14 +15,6 @@ it('renders correctly', () => {
     ).toJSON();
     expect(tree).toMatchSnapshot();
 });
-
-/*it('sets the correct date on the forms', () => {
-    const date = '2018-01-14';
-
-    const wrapper = shallow(<NewReportScreen/>);
-
-    expect(wrapper.instance().getDate()).toBe(date);
-});*/
 
 it('fetch finds data from the server', () => {
     const data = fetch(url + '/users/1/forms');
