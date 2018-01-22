2e5df8a4c0c8fe102f744e3404d8c6961868e515
Minna Laitinen <minna.maaria.laitinen@gmail.com>
2018-01-22 20:24:40 +0200

    tests modified (#36)

---
+++ __tests__/TemplateScreen.js
@@ -0,0 +1,34 @@
+import 'react-native';
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import { ActivityIndicator } from 'react-native';
+import renderer from 'react-test-renderer';
+
+import TemplateScreen from '../src/screens/TemplateScreen';
+
+configure({ adapter: new Adapter() });
+
+it('renders correctly', () => {
+    const tree = renderer.create(
+        <TemplateScreen />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+
+describe('<TemplateScreen />', () => {
+    describe('isLoading', () => {
+        it('should render a <ActivityIndicator /> if true', () => {
+            const templateScreen = shallow(<TemplateScreen />);
+            templateScreen.setState({ isLoading: true });
+            expect(templateScreen.find(ActivityIndicator).length).toBe(1);
+        });
+
+        it('should not render a <ActivityIndicator /> if false', () => {
+            const templateScreen = shallow(<TemplateScreen />);
+            templateScreen.setState({ isLoading: false });
+            expect(templateScreen.find(ActivityIndicator).length).toBe(0);
+        });
+    });
+});
