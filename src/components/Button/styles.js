0d3a7c6f6ef6b1cbc49b7a44e934d4061369d4c8
Elisa <elisa.naskali@gmail.com>
2018-01-26 13:53:23 +0200

    Login screen should render correctly on smaller resolution too

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -4,21 +4,37 @@ const styles = EStyleSheet.create({
 
     buttonContainer: {
         backgroundColor: '#9dcbe5',
-        marginTop:30,
         paddingTop:10,
         paddingBottom:10,
-        marginLeft:115,
-        marginRight:115,
-        marginBottom: 60,
         borderRadius:10,
         borderWidth: 1,
         borderColor: '#9dcbe5',
+        '@media (max-width: 350)': {
+            marginTop: 20,
+            marginLeft: 90,
+            marginRight: 90,
+            paddingTop: 6,
+            paddingBottom: 6,
+        },
+        '@media (min-width: 350)': {
+            marginTop: 30,
+            marginLeft: 115,
+            marginRight: 115,
+            marginBottom: 60,
+            paddingTop: 10,
+            paddingBottom: 10,
+        }
     },
     text: {
         color: '#274752',
         fontFamily: '$primaryFont',
-        fontSize: 15,
         textAlign: 'center',
+        '@media (max-width: 350)': {
+            fontSize: 12,
+        },
+        '@media (min-width: 350)': {
+            fontSize: 16,
+        }
     },
 });
 
