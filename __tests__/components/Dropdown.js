1f05a6dc9e2113318c25665ba8164453d0364e38
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2018-03-29 22:56:05 +0300

    Changed " to '.

--- __tests__/components/Dropdown.js
+++ __tests__/components/Dropdown.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import { wrapper, configure } from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 
@@ -10,7 +10,7 @@ configure({ adapter: new Adapter() });
 
 describe('Dropdown', () => {
 
-    const wrapper = renderer.create(<Dropdown disabled={false} defaultValue={"some value"} options={['test1', 'test2']} />);
+    const wrapper = renderer.create(<Dropdown disabled={false} defaultValue={'some value'} options={['test1', 'test2']} />);
     const inst = wrapper.getInstance();
 
     it('renders correctly', () => {
@@ -18,7 +18,7 @@ describe('Dropdown', () => {
         expect(tree).toMatchSnapshot();
     });
 
-    describe("onSelect(0, 'test1')", () => {
+    describe('onSelect(0, "test1")', () => {
         it('should set state value to test1 when called', () => {
             inst.onSelect(0, 'test1');
             expect(inst.state.value).toBe('test1');
