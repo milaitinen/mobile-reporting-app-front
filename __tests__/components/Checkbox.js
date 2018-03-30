92f1ef2fd122dbd03d45c500159087a0acad6ed5
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2018-03-30 17:48:29 +0300

    Added more tests to checkbox component.

--- __tests__/components/Checkbox.js
+++ __tests__/components/Checkbox.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import { configure } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 
@@ -8,14 +8,29 @@ import Checkbox from '../../src/components/Checkbox/Checkbox';
 
 configure({ adapter: new Adapter() });
 
-it('renders correctly', () => {
-    const tree = renderer.create(
-        <Checkbox title='some checkbox' editable={false} />
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+describe('Checkbox tests', () => {
+    it('renders correctly when not editable', () => {
+        const tree = renderer.create(
+            <Checkbox  title='some checkbox' editable={false} onPressFunction={jest.fn()}/>
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('renders correctly when editable', () => {
+        const tree = renderer.create(
+            <Checkbox  title='some checkbox' editable={true} onPressFunction={jest.fn()}/>
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('calls the onPressFunction when pressed', () => {
+        const mockPress = jest.fn();
+        const checkbox = shallow(
+            <Checkbox title='some checkbox' isChecked={false} editable={true} onPressFunction={mockPress}/>
+        );
+
+        const checkBox = checkbox.find('CheckBox');
+        checkBox.simulate('press');
+        expect(mockPress).toHaveBeenCalled();
+    });
 });
-
-
-
-
-
