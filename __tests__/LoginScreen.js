a00abc14685b758a24b359a0ffc3e66e56929e67
Minna <minna.maaria.laitinen@gmail.com>
2018-03-31 12:53:25 +0300

    Fixed LoginScreen test bug

--- __tests__/LoginScreen.js
+++ __tests__/LoginScreen.js
@@ -5,9 +5,10 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import { LoginScreen } from '../src/screens/LoginScreen';
-import { SignInButton } from '../src/components/Button';
 
 configure({ adapter: new Adapter() });
+
+jest.mock('../src/navigation/AppNavigation');
 
 it('renders correctly', () => {
     const tree = renderer.create(
@@ -15,10 +16,3 @@ it('renders correctly', () => {
     ).toJSON();
     expect(tree).toMatchSnapshot();
 });
-
-/*it('pressing the "Sign In" button works correctly', () => {
-    const opensTemplates = jest.fn();
-    const component = shallow(<SignInButton onPress={ opensTemplates }/>);
-    component.simulate( 'press' );
-    expect(opensTemplates).toHaveBeenCalled();
-});*/
