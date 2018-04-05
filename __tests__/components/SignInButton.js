6a5f12afc857732848317af66bae6925936380e9
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2018-04-05 16:34:51 +0300

    Test for pressing sign in button.

--- __tests__/components/SignInButton.js
+++ __tests__/components/SignInButton.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
-import { Touchable } from 'react-native';
 
 import SignInButton from '../../src/components/Button/SignInButton';
 
@@ -17,18 +16,9 @@ it('Sign in button renders correctly', () => {
     expect(tree).toMatchSnapshot();
 });
 
-/* TODO: fix to work
-jest.mock('../../src/components/Button/SignInButton', () => {
-    const mockComponent = require('react-native');
-    return mockComponent('../../src/components/Button/SignInButton');
-});
-
 it('pressing the Sign in button works correctly', () => {
-    const openTemplates = jest.fn();
-    const component = renderer.create(
-        <SignInButton onPress={ openTemplates }/>
-    ).getInstance();
-
-    component.simulate( 'press' );
-    expect(openTemplates).toHaveBeenCalled();
-});*/
+    const mockPress = jest.fn();
+    const wrapper = shallow(<SignInButton onPress={mockPress}/>);
+    wrapper.find('TouchableHighlight').simulate( 'press' );
+    expect(mockPress).toHaveBeenCalled();
+});
