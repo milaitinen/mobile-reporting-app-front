594cd961fb4b906d94dd998fe4fa77414a3fb367
Minna <minna.maaria.laitinen@gmail.com>
2018-04-11 01:38:36 +0300

    More tests in loginScreen

--- __tests__/screens/LoginScreen.js
+++ __tests__/screens/LoginScreen.js
@@ -6,6 +6,8 @@ import configureStore from 'redux-mock-store';
 import Adapter from 'enzyme-adapter-react-16';
 
 import connectedLoginScreen, { LoginScreen } from '../../src/screens/LoginScreen';
+import * as userTypes from '../../src/redux/actions/user';
+import * as connectionTypes from '../../src/redux/actions/connection';
 
 configure({ adapter: new Adapter() });
 
@@ -26,7 +28,7 @@ describe('Login screen', () => {
         expect(tree).toMatchSnapshot();
     });
 
-    it('calls logIn function correctly', async () => {
+    it('calls dispatch in logIn function correctly', async () => {
 
         jest.unmock('../../src/screens/api');
         const api = require.requireActual('../../src/screens/api');
@@ -35,14 +37,24 @@ describe('Login screen', () => {
         const dispatch = jest.fn();
         const navigation = { state: { params: { isEditable: true } }, dispatch: dispatch };
 
-        const props = {
-            navigation,
-            dispatch
-        };
-
-        const wrapper = shallow(<LoginScreen {...props} />);
+        const wrapper = shallow(<LoginScreen navigation={navigation} dispatch={dispatch} />);
         await wrapper.find('SignInButton').simulate('press');
 
-        expect(dispatch).toHaveBeenCalled();
+        expect(dispatch).toHaveBeenCalledWith({ type: userTypes.INSERT_TOKEN, token: true });
+    });
+
+    it('does not call dispatch in logIn function when token is undefined', async () => {
+
+        jest.unmock('../../src/screens/api');
+        const api = require.requireActual('../../src/screens/api');
+        api.login = jest.fn(() => new Promise(resolve => resolve()));
+
+        const dispatch = jest.fn();
+        const navigation = { state: { params: { isEditable: true } }, dispatch: dispatch };
+
+        const wrapper = shallow(<LoginScreen navigation={navigation} dispatch={dispatch} />);
+        await wrapper.find('SignInButton').simulate('press');
+
+        expect(dispatch).toHaveBeenLastCalledWith({ type: connectionTypes.SET_INITIAL_CONNECTION, isConnected: undefined });
     });
 });
