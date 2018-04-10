7ba7fa78182c698b7f5347578f2f31aeb7a3119d
Minna <minna.maaria.laitinen@gmail.com>
2018-04-11 00:06:38 +0300

    logIn test runs correctly

--- __tests__/screens/LoginScreen.js
+++ __tests__/screens/LoginScreen.js
@@ -6,31 +6,43 @@ import configureStore from 'redux-mock-store';
 import Adapter from 'enzyme-adapter-react-16';
 
 import connectedLoginScreen, { LoginScreen } from '../../src/screens/LoginScreen';
-import { url } from '../../src/screens/urlsetting';
 
 configure({ adapter: new Adapter() });
-jest.mock('../../src/components/OfflineNotice', () => 'OfflineNotice');
 
+jest.mock('../../src/components/OfflineNotice', () => 'OfflineNotice');
 jest.mock('../../src/navigation/AppNavigation');
 
 describe('Login screen', () => {
     it('renders correctly', () => {
+
         const mockStore = configureStore();
         const initialState = {};
         const store = mockStore(initialState);
+
         const tree = renderer.create(
             <connectedLoginScreen store={store} />
         ).toJSON();
+
         expect(tree).toMatchSnapshot();
     });
 
-    it('calls logIn function correctly', () => {
-        const mockDispatch = jest.fn();
-        const navigation = { state: { params: { isEditable: true } }, dispatch: mockDispatch };
-        const wrapper = shallow(<LoginScreen navigation={navigation} dispatch={mockDispatch}/>);
-        wrapper
-        wrapper.instance().logIn();
-        expect(mockDispatch).toHaveBeenCalled();
+    it('calls logIn function correctly', async () => {
 
+        jest.unmock('../../src/screens/api');
+        const api = require.requireActual('../../src/screens/api');
+        api.login = jest.fn(() => new Promise(resolve => resolve(true)));
+
+        const dispatch = jest.fn();
+        const navigation = { state: { params: { isEditable: true } }, dispatch: dispatch };
+
+        const props = {
+            navigation,
+            dispatch
+        };
+
+        const wrapper = shallow(<LoginScreen {...props} />);
+        await wrapper.find('SignInButton').simulate('press');
+
+        expect(dispatch).toHaveBeenCalled();
     });
 });
